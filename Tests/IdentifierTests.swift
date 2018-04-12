86472ba8ff01b7ce0782ba7735c4f7c2aae03095
Ali Akhtarzada <ali.akhtarzada@gmail.com>
2018-04-12 12:36:19 +0200

    refactor and add some more identifier tests

--- Tests/IdentifierTests.swift
+++ Tests/IdentifierTests.swift
@@ -10,52 +10,99 @@ import Quick
 
 class IdentifierTests: QuickSpec {
 
+    let testCountryCode = "47"
+    lazy var testCountryCodeWithPlus = "+" + testCountryCode
+    lazy var testCountryCodeWith00 = "00" + testCountryCode
+    let testNumberRaw = "12345678"
+    let testNumerWithSpaces = "1 2 3 4 5 6 7 8"
+    lazy var testNormalizedNumber = testCountryCodeWithPlus + testNumberRaw
+    lazy var testPhoneNumber = PhoneNumber(countryCode: testCountryCodeWithPlus, number: testNumberRaw)!
+    let testEmailRaw = "hello@bubble.com"
+    lazy var testEmailAddress = EmailAddress(testEmailRaw)!
+
     override func spec() {
 
         describe("Identifier") {
 
             it("Should have original email") {
-                let emailAddress = "hello@bubble.com"
-                let identifier = Identifier(EmailAddress(emailAddress)!)
-                expect(identifier.originalString) == emailAddress
+                let identifier = Identifier(self.testEmailAddress)
+                expect(identifier.originalString) == self.testEmailRaw
             }
 
             it("Should have original phone number") {
-                let countryCode = "+47"
-                let number = "01020304"
-                let identifier = Identifier(PhoneNumber(countryCode: countryCode, number: number)!)
-                expect(identifier.originalString) == countryCode + number
+                let identifier = Identifier(self.testPhoneNumber)
+                expect(identifier.originalString) == self.testCountryCodeWithPlus + self.testNumberRaw
             }
+        }
+
+        describe("serelization") {
+
+            it("should serialize phone numbers and unserialize to same number") {
+                let serialized = Identifier(self.testPhoneNumber).serializedString
+                let identifier = Identifier(serializedString: serialized)!
+                let expected = Identifier(self.testPhoneNumber)
+                expect(identifier) == expected
+            }
+
+            it("should serialize emails and unserialize to same number") {
+                let serialized = Identifier(self.testEmailAddress).serializedString
+                let identifier = Identifier(serializedString: serialized)!
+                let expected = Identifier(self.testEmailAddress)
+                expect(identifier) == expected
+            }
+        }
+
+        describe("local ID") {
+
+            it("should return correct email identiier") {
+                let expected = Identifier(self.testEmailAddress)
+                let localID = expected.localID()
+                let identifier = Identifier(localID: localID)!
+                expect(identifier) == expected
+            }
+
+            it("should return correct phone number identiier") {
+                let expected = Identifier(self.testPhoneNumber)
+                let localID = expected.localID()
+                let identifier = Identifier(localID: localID)!
+                expect(identifier) == expected
+            }
+
+            it("should return same id with mutiple invocations on phone number") {
+                let id1 = Identifier(self.testPhoneNumber).localID()
+                let id2 = Identifier(self.testPhoneNumber).localID()
+                expect(id1) == id2
+            }
+
+            it("should return same id with mutiple invocations on email") {
+                let id1 = Identifier(self.testEmailAddress).localID()
+                let id2 = Identifier(self.testEmailAddress).localID()
+                expect(id1) == id2
+            }
+
         }
 
         describe("Normalizing PhoneNumber") {
 
-            let testCountryCode = "47"
-            let testCountryCodeWithPlus = "+" + testCountryCode
-            let testCountryCodeWith00 = "00" + testCountryCode
-            let testNumber = "12345678"
-            let testNumerWithSpaces = "1 2 3 4 5 6 7 8"
-            let expectedNumber = testCountryCodeWithPlus + testNumber
-
             it("Should automatically add a + to phone numbers that don't have one") {
-                let phoneNumber = PhoneNumber(countryCode: testCountryCode, number: testNumber)
-                expect(phoneNumber?.normalizedString) == expectedNumber
-                expect(phoneNumber?.normalizedValue.countryCode) == testCountryCodeWithPlus
-                expect(phoneNumber?.normalizedValue.number) == testNumber
+                let phoneNumber = PhoneNumber(countryCode: self.testCountryCode, number: self.testNumberRaw)
+                expect(phoneNumber?.normalizedString) == self.testNormalizedNumber
+                expect(phoneNumber?.normalizedValue.countryCode) == self.testCountryCodeWithPlus
+                expect(phoneNumber?.normalizedValue.number) == self.testNumberRaw
             }
 
             it("Should leave number with + intact") {
-                let phoneNumber = PhoneNumber(countryCode: testCountryCodeWithPlus, number: testNumber)
-                expect(phoneNumber?.normalizedString) == expectedNumber
-                expect(phoneNumber?.normalizedValue.countryCode) == testCountryCodeWithPlus
-                expect(phoneNumber?.normalizedValue.number) == testNumber
+                let phoneNumber = PhoneNumber(countryCode: self.testCountryCodeWithPlus, number: self.testNumberRaw)
+                expect(phoneNumber?.normalizedString) == self.testNormalizedNumber
+                expect(phoneNumber?.normalizedValue.countryCode) == self.testCountryCodeWithPlus
+                expect(phoneNumber?.normalizedValue.number) == self.testNumberRaw
             }
 
             it("Should strip 00 and add a +") {
-                let phoneNumber = PhoneNumber(countryCode: testCountryCodeWith00, number: testNumber)
-                expect(phoneNumber?.normalizedString) == expectedNumber
-                expect(phoneNumber?.normalizedValue.countryCode) == testCountryCodeWithPlus
-                expect(phoneNumber?.normalizedValue.number) == testNumber
+                let phoneNumber = PhoneNumber(countryCode: self.testCountryCodeWith00, number: self.testNumberRaw)
+                expect(phoneNumber?.normalizedString) == self.testNormalizedNumber
+                expect(phoneNumber?.normalizedValue.countryCode) == self.testCountryCodeWithPlus
+                expect(phoneNumber?.normalizedValue.number) == self.testNumberRaw
             }
 
             it("Should be nil if number has non valid phone characters in it") {
@@ -68,7 +115,7 @@ class IdentifierTests: QuickSpec {
             }
 
             it("Should remove spaces") {
-                expect(PhoneNumber(countryCode: "+47", number: testNumerWithSpaces)?.normalizedString) == expectedNumber
+                expect(PhoneNumber(countryCode: "+47", number: self.testNumerWithSpaces)?.normalizedString) == self.testNormalizedNumber
             }
         }
     }
