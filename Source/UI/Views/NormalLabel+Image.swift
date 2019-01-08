60f6f118a92a5ebc3b4830ce1e29356c263e57e6
Oussama Sarhraoui <oussama.sarhraoui@schibsted.com>
2019-01-08 10:41:03 +0100

    Add image as attachement

---
+++ Source/UI/Views/NormalLabel+Image.swift
@@ -0,0 +1,64 @@
+//
+//  IconLabel.swift
+//  SchibstedAccount
+//
+//  Created by Oussama Sarhraoui on 2019-01-07.
+//  Copyright © 2019 Schibsted. All rights reserved.
+//
+
+
+extension NormalLabel {
+    /**
+     This function adding image with text on label.
+     
+     - parameter text: The text to add
+     - parameter image: The image to add
+     - parameter imageBehindText: A boolean value that indicate if the imaga is behind text or not
+     - parameter keepPreviousText: A boolean value that indicate if the function keep the actual text or not
+     */
+    func addTextWithImage(text: String, image: UIImage, imageBehindText: Bool, keepPreviousText: Bool) {
+        let lAttachment = NSTextAttachment()
+        lAttachment.image = image
+        
+        // 1pt = 1.32px
+        let lFontSize = round(self.font.pointSize * 1.32)
+        let lRatio = image.size.width / image.size.height
+        
+        lAttachment.bounds = CGRect(x: 0, y: ((self.font.capHeight - lFontSize) / 2).rounded(), width: lRatio * lFontSize, height: lFontSize)
+        
+        let lAttachmentString = NSAttributedString(attachment: lAttachment)
+        
+        if imageBehindText {
+            let lStrLabelText: NSMutableAttributedString
+            
+            if keepPreviousText, let lCurrentAttributedString = self.attributedText {
+                lStrLabelText = NSMutableAttributedString(attributedString: lCurrentAttributedString)
+                lStrLabelText.append(NSMutableAttributedString(string: text))
+            } else {
+                lStrLabelText = NSMutableAttributedString(string: text)
+            }
+            
+            lStrLabelText.append(lAttachmentString)
+            self.attributedText = lStrLabelText
+        } else {
+            let lStrLabelText: NSMutableAttributedString
+            
+            if keepPreviousText, let lCurrentAttributedString = self.attributedText {
+                lStrLabelText = NSMutableAttributedString(attributedString: lCurrentAttributedString)
+                lStrLabelText.append(NSMutableAttributedString(attributedString: lAttachmentString))
+                lStrLabelText.append(NSMutableAttributedString(string: text))
+            } else {
+                lStrLabelText = NSMutableAttributedString(attributedString: lAttachmentString)
+                lStrLabelText.append(NSMutableAttributedString(string: text))
+            }
+            
+            self.attributedText = lStrLabelText
+        }
+    }
+    
+    func removeImage() {
+        let text = self.text
+        self.attributedText = nil
+        self.text = text
+    }
+}
